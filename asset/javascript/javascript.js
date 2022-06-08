1c2aeef5bb925c9d0424c82d1ee67aae47f04acb
tahir-arslan <101910849+tahir-arslan@users.noreply.github.com>
2022-06-08 11:52:31 -0400

    populated work hours and planner layout

--- asset/javascript/javascript.js
+++ asset/javascript/javascript.js
@@ -1,5 +1,22 @@
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
 
-let date = (moment().format('MMMM Do YYYY'));
-$("#currentDay").append(date);
+$("#currentDay").append(moment().format('MMMM Do YYYY'));
+
+// populate planner (working hours: 9am - 5pm)
+for (let i = 0; i < 9; i++) {
+    // add row
+    let setRow = $("<div class='row'></div>");
+    $(".container").append(setRow);
+    // add time
+    let hourEl = moment().hour(i + 9).format('H A');
+    let hourCol = $(`<div class="col-1 hour" data-index="${i}"></div>`);
+    hourCol.append(hourEl);
+    setRow.append(hourCol);
+    // add editable text area (no click event)
+    let editTask = $(`<div class='col-10 description data-index="${i}"'></div>`)
+    setRow.append(editTask);
+    // add save button
+    let saveBtnEl = $(`<button class="col-1 saveBtn" data-index="${i}"><span class="oi oi-lock-unlocked"></span></button>`);
+    setRow.append(saveBtnEl);
+}
