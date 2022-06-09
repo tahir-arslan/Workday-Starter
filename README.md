c0c8c9f26ebab8890f6a6a0fb2e1179be53517c4
tahir-arslan <101910849+tahir-arslan@users.noreply.github.com>
2022-06-08 23:34:57 -0400

    added editable text, save/unsave button, and color coded blocks

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Work Day Scheduler Starter Code
+
+A day planner created by primarily using third-party API's, allowing you to add tasks for the day. Tasks can be saved or unsaved, and are persistant if saved. Each hour is color coded according to that time block relative to the current time.
+
+#### Website Preview
+![](assets/images/screenshot.png)
+
+## Link 
+The link to the deployed website can be found here:
+[Work Day Scheduler](https://tahir-arslan.github.io/Workday-Starter/)
--- asset/javascript/javascript.js
+++ asset/javascript/javascript.js
@@ -1,22 +1,82 @@
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
+let storedTasks = JSON.parse(localStorage.getItem("taskList"));
+let currentHour = moment().format("hA");
+let past = true;
+let present = false;
 
 $("#currentDay").append(moment().format('MMMM Do YYYY'));
+if (!storedTasks) {
+    storedTasks = {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        9: ""
+    }
+}
 
 // populate planner (working hours: 9am - 5pm)
 for (let i = 0; i < 9; i++) {
+    let hourEl = moment().hour(i + 9).format('hA');
+
     // add row
     let setRow = $("<div class='row'></div>");
     $(".container").append(setRow);
+
     // add time
-    let hourEl = moment().hour(i + 9).format('H A');
     let hourCol = $(`<div class="col-1 hour" data-index="${i}"></div>`);
     hourCol.append(hourEl);
     setRow.append(hourCol);
-    // add editable text area (no click event)
-    let editTask = $(`<div class='col-10 description data-index="${i}"'></div>`)
+
+    // add editable text area
+    let editTask = $(`<textarea class='col-10 description data-index="${i}"'></textarea>`)
+    editTask.text(storedTasks[i]);
     setRow.append(editTask);
+
+    // colorize block according to past, present, future
+    if (hourEl === currentHour) {
+        past = false;
+        present = true;
+    }
+    if (past) {
+        editTask.addClass("past");
+    } else if (present) {
+        editTask.addClass("present");
+        present = false;
+    } else {
+        editTask.addClass("future");
+    }
+
     // add save button
-    let saveBtnEl = $(`<button class="col-1 saveBtn" data-index="${i}"><span class="oi oi-lock-unlocked"></span></button>`);
+    let saveBtnEl = $(`<button class="col-1 btn saveBtn" data-index="${i}">`);
+    if (storedTasks[i].length) {
+        saveBtnEl.append(`<p class="fas fa-lock" data-index="${i}">`);
+        editTask.prop('disabled', true);
+    } else {
+        saveBtnEl.append(`<p class="fas fa-unlock-alt" data-index="${i}">`);
+    }
     setRow.append(saveBtnEl);
 }
+
+// click event
+$(".saveBtn").click(function() {
+    let index = $(this).attr("data-index");
+    if (storedTasks[index]) {
+        $(`textarea[data-index="${index}"]`).prop("disabled", false);
+        storedTasks[index] = "";
+        $(`p[data-index="${index}"]`).removeClass("fa-lock").addClass("fa-unlocked-alt");
+        localStorage.setItem("taskList", JSON.stringify(storedTasks));
+    } else {
+        let txt = $(`textarea[data-index="${index}"]`)
+        if (txt.val()) {
+            storedTasks[index] = txt.val();
+            txt.prop("disabled", true);
+            $(`i[data-index="${index}"]`).removeClass("fa-unlocked-alt").addClass("fa-lock");
+            localStorage.setItem("taskList", JSON.stringify(storedTasks));
+        }
+    }
+})
